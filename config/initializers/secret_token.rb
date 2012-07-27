5aebb06e20f1091ab74c4804ab2e13a140f7c876
LuciferUltraM <luciferultram@gmail.com>
2012-07-28 04:06:34 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RobotMakathon::Application.config.secret_token = '2a5148d131e99139a107c3dc53da515c5e8284bbafc1fdb9f6fa72a36223217f09ee7d72def52665a67b2362a9ba011cc2e47777a9096f62c55245eca1b38e34'
