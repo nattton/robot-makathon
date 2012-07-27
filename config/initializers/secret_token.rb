188feb66ecee320feb8061719c3f2844fc4d0b50
LuciferUltraM <luciferultram@gmail.com>
2012-07-28 04:45:59 +0700

    Initial Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RobotMakathon::Application.config.secret_token = 'd22485b830424de6b445b46aa1548b978536c37b8a8e3223e7658bcb251c4a7834bff1c7a3e451d0676f379513db0446849700152e8814e1f40d97e33a447c7b'
